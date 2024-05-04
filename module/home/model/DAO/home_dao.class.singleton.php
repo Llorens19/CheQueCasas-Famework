3a4b4ebcaacff4cf33e807893fb6420dffecab22
Diego Llorens <dllorens21@gmail.com>
2024-05-04 15:05:22 +0200

    Migrar_funciones_BD_DAO

--- module/home/model/DAO/home_dao.class.singleton.php
+++ module/home/model/DAO/home_dao.class.singleton.php
@@ -22,5 +22,63 @@ class home_dao
 		return $db->listar($stmt);
 	}
 
+	public function select_operations($db)
+	{
+		$sql = "SELECT * FROM `operations` ORDER BY id_operations ASC";
+
+		$stmt = $db->ejecutar($sql);
+		return $db->listar($stmt);
+	}
+
+	public function select_city($db)
+	{
+		$sql = "SELECT * FROM `city` ORDER BY id_city ASC";
+
+		$stmt = $db->ejecutar($sql);
+		return $db->listar($stmt);
+	}
+
+	public function select_most_viewed($db)
+	{
+		$sql = "SELECT *
+		FROM building b, type t, operations o, city c
+		WHERE b.id_type = t.id_type 
+		AND b.id_operations = o.id_operations 
+		AND b.id_city = c.id_city 
+        order by b.views desc limit 5";
+
+		$stmt = $db->ejecutar($sql);
+		return $db->listar($stmt);
+	}
+
+	public function select_lasts_views($array, $db)
+	{
+
+		$sql = "SELECT *
+                    FROM building b, type t, operations o, city c
+                    WHERE b.id_type = t.id_type
+                    AND b.id_operations = o.id_operations
+                    AND b.id_city = c.id_city ";
+
+		if (!empty($array)) {
+			$ids = implode(',', $array);
+			$sql .= " AND b.id_building IN ($ids)";
+		}
+
+		$stmt = $db->ejecutar($sql);
+		return $db->listar($stmt);
+	}
+
+	
+
+	function select_images($db)
+	{
+		$sql = "SELECT *
+		FROM image";
+
+		$stmt = $db->ejecutar($sql);
+		return $db->listar($stmt);
+	}
+
 	
 }
