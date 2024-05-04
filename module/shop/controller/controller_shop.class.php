26626d646f0e5f3d2830054e5860bdb8ec914b2c
Diego Llorens <dllorens21@gmail.com>
2024-05-04 15:29:48 +0200

    Controlador_shop_migrado

---
+++ module/shop/controller/controller_shop.class.php
@@ -0,0 +1,45 @@
+<?php
+
+
+
+    class controller_shop {
+
+
+
+        function view() {
+            common::load_view('top_page_shop.html', VIEW_PATH_SHOP . 'shop.html');
+        }
+
+        function load_buildings() {
+            echo json_encode(common::load_model('shop_model', 'get_buildings', [$_POST['filters'], $_POST['total_prod'], $_POST['items_page']]));
+        }
+
+        function details_building() {
+            echo json_encode(common::load_model('shop_model', 'get_details', $_POST['id_building']));
+        }
+        
+        function filters_shop_refill() {
+            echo json_encode(common::load_model('shop_model', 'get_filters_shop_refill', [$_POST['filters'],  $_POST['items_page']]));
+        }
+        function filters_table() {
+            echo json_encode(common::load_model('shop_model', 'get_filters_table'));
+        }
+
+        function count_click_details() {
+            echo json_encode(common::load_model('shop_model', 'get_count_click_details', $_POST['id'] ));
+        }
+        function total_prod() {
+            echo json_encode(common::load_model('shop_model', 'get_total_prod', $_POST['filters']));
+        }
+        function likes_user() {
+            echo json_encode(common::load_model('shop_model', 'get_likes_user', $_SESSION['username']));
+        }
+        function table_likes() {
+            echo json_encode(common::load_model('shop_model', 'get_table_likes'));
+        }
+
+        function action_like() {
+            echo json_encode(common::load_model('shop_model', 'get_action_like', [$_SESSION['username'], $_POST['id_building']]));
+        }
+
+    }
