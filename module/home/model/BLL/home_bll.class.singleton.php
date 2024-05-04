e62c6346ee729f720261d0771a64d850e50f9246
Diego Llorens <dllorens21@gmail.com>
2024-05-04 15:03:17 +0200

    Metodos_BLL_migrados

--- module/home/model/BLL/home_bll.class.singleton.php
+++ module/home/model/BLL/home_bll.class.singleton.php
@@ -30,5 +30,93 @@
 				return "error";
 			}
 		}
+
+		public function get_operations_BLL() {
+
+			try {
+				$select_type = $this -> dao -> select_operations($this -> db);
+			} catch (Exception $e) {
+				return "error";
+			}
+	
+			if (!empty($select_type)) {
+				return $select_type;
+
+			} else {
+				return "error";
+			}
+		}
+
+		public function get_city_BLL() {
+
+			try {
+				$select_type = $this -> dao -> select_city($this -> db);
+			} catch (Exception $e) {
+				return "error";
+			}
+	
+			if (!empty($select_type)) {
+				return $select_type;
+
+			} else {
+				return "error";
+			}
+		}
+
+
+
+		public function get_most_viewed_BLL() {
+
+
+			try {
+				$data_building = $this -> dao -> select_most_viewed($this -> db);
+			} catch (Exception $e) {
+				return "error";
+			}
+			try {
+				
+				$data_img = $this -> dao -> select_images($this -> db);
+			} catch (Exception $e) {
+				return "error";
+			}
+		
+			if (!empty($data_building || $data_img)) {
+				$rdo = array();
+				$rdo[0]= $data_building;
+				$rdo[1]= $data_img;
+		
+				return $rdo;
+			} else {
+				return "error";
+			}
+
+
+		}
+
+		public function get_lasts_views_BLL($args) {
+
+			try {
+				$data_building = $this -> dao -> select_lasts_views($this -> db, $args);
+			} catch (Exception $e) {
+				return "error";
+			}
+			try {
+				
+				$data_img = $this -> dao -> select_images($this -> db);
+			} catch (Exception $e) {
+				return "error";
+			}
+		
+			if (!empty($data_building || $data_img)) {
+				$rdo = array();
+				$rdo[0]= $data_building;
+				$rdo[1]= $data_img;
+		
+				return $rdo;
+			} else {
+				return "error";
+			}
+
+		}
 		
 	}
