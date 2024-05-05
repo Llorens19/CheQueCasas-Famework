fe716431651f72534e7bc60f67feb4afdaed1c6e
Diego Llorens <dllorens21@gmail.com>
2024-05-05 17:13:54 +0200

    Controller_login_migrado

---
+++ module/login/controller/controller_login.class.php
@@ -0,0 +1,44 @@
+<?php
+class controller_login
+{
+
+    function register()
+    {
+        error_log("register");
+        echo json_encode(common::load_model('login_model', 'get_register', [
+            [$_POST['email']],
+            [$_POST['username']],
+            [$_POST['username'], $_POST['email'], $_POST['password'], $_POST['name'], $_POST['surname'], $_POST['tlf']]
+        ]));
+    }
+    function login()
+    {
+        echo json_encode(common::load_model('login_model', 'get_login', [[$_POST['username']],[$_POST['password']]]));
+    }
+
+    function logout()
+    {
+        // echo json_encode(common::load_model('login_model', 'get_logout', [$_SESSION['username'], $_SESSION['tiempo']]));
+        echo json_encode("Done");
+    }
+
+    function data_user()
+    {
+    echo json_encode(common::load_model('login_model', 'get_data_user', $_POST['token']));
+    }
+
+    function actividad()
+    {
+        echo json_encode(common::load_model('login_model', 'get_actividad', $_SESSION["tiempo"]));
+    }
+    function controluser()
+    {
+        echo json_encode(common::load_model('login_model', 'get_controluser', [[$_POST["access_token"], $_POST["refresh_token"]],
+        [$_SESSION['username']]]));
+    }
+
+    function refresh_cookie()
+    {
+        echo json_encode(common::load_model('login_model', 'get_refresh_cookie'));
+    }
+}
