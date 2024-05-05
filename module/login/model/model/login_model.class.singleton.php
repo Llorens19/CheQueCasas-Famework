23929f4c954e71de067cbf159305a1e9190248c8
Diego Llorens <dllorens21@gmail.com>
2024-05-05 17:52:07 +0200

    model_login

---
+++ module/login/model/model/login_model.class.singleton.php
@@ -0,0 +1,42 @@
+<?php
+    class login_model {
+
+        private $bll;
+        static $_instance;
+        
+        function __construct() {
+            $this -> bll = login_bll::getInstance();
+        }
+
+        public static function getInstance() {
+            if (!(self::$_instance instanceof self)) {
+                self::$_instance = new self();
+            }
+            return self::$_instance;
+        }
+
+        public function get_login($args) {
+            return $this -> bll -> get_login_BLL($args);
+        }
+
+        public function get_logout($args) {
+            return $this -> bll -> get_logout_BLL($args);
+        }
+
+        public function get_data_user($args) {
+            return $this -> bll -> get_data_user_BLL($args);
+        }
+        public function get_actividad($args) {
+            return $this -> bll -> get_actividad_BLL($args);
+        }
+        public function get_controluser($args) {
+            return $this -> bll -> get_controluser_BLL($args);
+        }
+
+        public function get_refresh_cookie() {
+            return $this -> bll -> get_refresh_cookie_BLL();
+        }
+
+
+
+    }
