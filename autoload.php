af71a264054722b6c3b96f3a1dad76083e5a878f
Llorens19 <llorenssorianodiego@gmail.com>
2024-05-11 12:32:57 +0200

    Autoload_mejora

--- autoload.php
+++ autoload.php
@@ -1,30 +1,53 @@
 <?php
-    require_once("paths.php");
-    require_once(SITE_ROOT . 'model/middleware_auth.php');
-    
-    spl_autoload_extensions('.php,.inc.php,.class.php,.class.singleton.php');
-    spl_autoload_register('loadClasses'); // Cargamos las clases que necesitemos
-    
-    function loadClasses($className) {
-        $breakClass = explode('_', $className);
-        $modelName = "";
-        
-        if (isset($breakClass[1])) {
-            $modelName = strtoupper($breakClass[1]);
-        }
-        
-        if (file_exists(SITE_ROOT . 'module/' . $breakClass[0] . '/model/'. $modelName . '/' . $className . '.class.singleton.php')) {
-            set_include_path('module/' . $breakClass[0] . '/model/' . $modelName.'/');
-            spl_autoload($className);
-        }else if (file_exists(SITE_ROOT . 'model/' . $className . '.class.singleton.php')){
-            set_include_path(SITE_ROOT . 'model/');
-            spl_autoload($className);
-        }else if (file_exists(SITE_ROOT . 'model/' . $className . '.class.php')){
-            set_include_path(SITE_ROOT . 'model/');
-            spl_autoload($className);
-        }else if (file_exists(SITE_ROOT . 'utils/' . $className . '.inc.php')) {
-            set_include_path(SITE_ROOT . 'utils/');
-            spl_autoload($className);
-        }
-        
+require_once("paths.php");
+require_once(SITE_ROOT . 'model/middleware_auth.php');
+
+spl_autoload_extensions('.php,.inc.php,.class.php,.class.singleton.php');
+spl_autoload_register('loadClasses');
+
+function loadClasses($className)
+{
+    $classExplode = explode('_', $className);
+    $module = $classExplode[0];
+    $option = '';
+    error_log("444444444444444444444444444444444444444444444444444");
+    error_log($className);
+    error_log($module);
+
+
+    if (isset($classExplode[1])) {
+        $option = $classExplode[1];
+        error_log($option);
     }
+
+    $modulePath = SITE_ROOT . 'module/' . $module . '/';
+    $modelPath = SITE_ROOT . 'model/';
+    $utilsPath = SITE_ROOT . 'utils/';
+
+
+    $modules = simplexml_load_file('resources/modules.xml');
+    foreach ($modules as $row) {
+        if (in_array($module, (array) $row->uri)) { //Si el módulo que queremos cargar está en la lista
+
+            if (file_exists($modulePath . 'model/' . $option . '/' . $className . '.class.singleton.php')) {
+                set_include_path($modulePath . 'model/' . $option . '/');
+                spl_autoload($className);
+
+                error_log("////////////////////////////////////");
+                error_log($modulePath . 'model/' . $option . '/' . $className . '.class.singleton.php');
+            }
+        }
+    }
+
+
+    if (file_exists($modelPath . $className . '.class.singleton.php')) {
+        set_include_path($modelPath);
+        spl_autoload($className);
+    }
+
+
+    if (file_exists($utilsPath . $className . '.inc.php')) {
+        set_include_path($utilsPath);
+        spl_autoload($className);
+    }
+}
